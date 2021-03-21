039ff7c2c430fbe5dc2afb43e59f4e426448e4df
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-03-21 23:18:29 +0900

    Add files via upload

---
+++ src/weeks.java
@@ -0,0 +1,12 @@
+public class weeks {
+    public static void main(String[] args){
+
+//        배열 순서대로 출력하는 법
+
+    String[] weeks = {"월", "화", "수", "목", "금"};
+
+    for(int i = 0;i<weeks.length;i++)
+        System.out.print(weeks[i]);
+
+}
+}
