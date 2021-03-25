ce8dc43ea641027b00018934b8c5c3d34f751911
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-03-25 15:57:16 +0900

    Update scan.java

--- src/scan.java
+++ src/scan.java
@@ -1,4 +1,14 @@
 package com.company;
+import java.util.Scanner;
 
-public class scan {
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("정수 입력 : ");
+        int i = scan.nextInt();
+        System.out.println(i);
+
+    }
 }
